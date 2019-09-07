d83325cb8cf3e970954502df3bafd69de1788b1b
Kelly Deng <kelly.deng@stonybrook.edu>
2019-09-08 04:43:58 +0800

    first commit

---
+++ ttt/static/ttt/js/ttt.js
@@ -0,0 +1,56 @@
+$(function () {
+
+    var winner = ' ';
+    var grid = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
+
+    $('.box').click(function () {
+        play(this.id);
+    });
+
+    function fillGrid(grid) {
+        for (var i = 0; i < grid.length; i++) {
+            // document.getElementById(i).innerHTML = grid[i] == ' ' ? '-' : grid[i];
+            document.getElementById(i).innerHTML = grid[i];
+        }    
+    }
+
+    function play(id) {
+        
+        var box = document.getElementById(id);
+        
+        if(winner == ' ' && box.innerHTML == ' ') {
+            box.innerHTML = 'x';
+
+            data = {
+                "play": id,
+                "grid": grid,
+            }
+            fetch("/ttt/play/", {
+                method: "POST",
+                mode: "cors",
+                cache: "no-cache",
+                credentials: "same-origin",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                redirect: "follow",
+                referrer: "no-referrer",
+                body: JSON.stringify(data)
+            })
+            .then(response => {
+                return response.json();
+            })
+            .then(response => {
+                console.log(response);
+                grid = response.grid;
+                winner = response.winner;
+                fillGrid(grid);
+                if(winner != ' ' || !grid.includes(' '))
+                    document.getElementById("winner").innerHTML = "The winner is " + (winner != ' ' ? winner : "no one") + "."
+            })
+        }
+
+    }
+  
+    fillGrid(grid);
+  });
---
+++ ttt/views.py
@@ -0,0 +1,128 @@
+from datetime import date
+import json, random
+
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.template import loader
+from django.views.decorators.csrf import csrf_exempt
+
+# from .forms import NameForm
+
+# Create your views here.
+def index(request):
+    # return HttpResponse("HELLO FROM POSTS")
+
+    # return render(request, 'ttt/index.html')
+
+    # template = loader.get_template('ttt/index.html')
+    # return HttpResponse(template.render(request))
+
+    # print(request.POST.get('name'))
+
+    # name_form = NameForm()
+
+    name = ""
+    if request.method == "POST":
+        if request.POST.get('name') not in ['', None]:
+            name = request.POST.get('name')
+            # d = date.today()
+            context = {
+                "name": name,
+            }
+            return render(request, 'ttt/index.html', context)
+        else:
+            # r = requests.post('./play/', params=request.POST)
+            # r = urllib.urlopen('/play/', request.POST)
+            # print(r)
+            
+            # d = date.today()
+            context = {
+                "name": name + "test",
+             }
+            return render(request, 'ttt/index.html', context)
+    
+    return render(request, 'ttt/index.html')
+
+@csrf_exempt
+def play(request):
+    # play = request.POST.get('play')
+    data = json.loads(request.body.decode('utf-8'))
+    play = int(data['play'])
+    grid = data['grid']
+
+    def check_winner(grid):
+        winner = ' '
+
+        # all the winning combinations (horizontal, vertical, diagonal; top, mid, bottom; left, mid, right)
+        ht = grid[0] + grid[1] + grid[2]
+        hm = grid[3] + grid[4] + grid[5]
+        hb = grid[6] + grid[7] + grid[8]
+
+        vl = grid[0] + grid[3] + grid[6]
+        vm = grid[1] + grid[4] + grid[7]
+        vr = grid[2] + grid[5] + grid[8]
+        
+        dl = grid[0] + grid[4] + grid[8]
+        dr = grid[2] + grid[4] + grid[6]
+
+        winning_combos = [ht, hm, hb, vl, vm, vr, dl, dr]
+
+        if 'XXX' in winning_combos:
+            winner = 'X'
+        elif 'OOO' in winning_combos:
+            winner = 'O'
+
+        return winner
+
+    def find_empty(grid, x, y, z):
+        for i in range(x, y, z):
+                if grid[i] == ' ':
+                    grid[i] = 'O'
+
+    def make_move(grid):
+        ht = grid[0] + grid[1] + grid[2]
+        hm = grid[3] + grid[4] + grid[5]
+        hb = grid[6] + grid[7] + grid[8]
+
+        vl = grid[0] + grid[3] + grid[6]
+        vm = grid[1] + grid[4] + grid[7]
+        vr = grid[2] + grid[5] + grid[8]
+        
+        dl = grid[0] + grid[4] + grid[8]
+        dr = grid[2] + grid[4] + grid[6]
+
+        action_combos = [' XX', 'X X', 'XX ', ' OO', 'O O', 'OO ']
+        if ht in action_combos:
+            find_empty(grid, 0, 3, 1)
+        elif hm in action_combos:
+            find_empty(grid, 3, 6, 1)
+        elif hb in action_combos:
+            find_empty(grid, 6, 9, 1)
+        elif vl in action_combos:
+            find_empty(grid, 0, 7, 3)
+        elif vm in action_combos:
+            find_empty(grid, 1, 8, 3)
+        elif vr in action_combos:
+            find_empty(grid, 2, 9, 3)
+        elif dl in action_combos:
+            find_empty(grid, 0, 9, 4)
+        elif dr in action_combos:
+            find_empty(grid, 2, 7, 2)
+        else:
+            i, j = [random.randint(0, 8), 0]
+            print(i)
+            while grid[i] != ' ' and j < 9:
+                i = random.randint(0, 8)
+                print(i)
+                j += 1
+            grid[i] = 'O'
+
+    grid[play] = 'X'    
+    winner = check_winner(grid)
+    
+    if winner == ' ':
+        make_move(grid)
+        # print(grid)
+        winner = check_winner(grid)
+
+    return JsonResponse({"grid": grid, "winner": winner})
